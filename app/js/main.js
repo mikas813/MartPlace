c6c984652f56c76742bb2b7f8725fa687bc98ad9
mikas813 <mishalas@icloud.com>
2019-12-15 10:02:07 +0000

    added featured slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,35 @@
 $(function () {
  //add an 'id='rateYo' 
-  $("#rateYo").rateYo({
+  $(".rate-star").rateYo({
       starWidth: "15px",
+      readOnly: true,
       rating: 4.5
   });
  
-});
-$('.featured__slider-items').slick({
+  //top main-page title slider!
+  $('.slider__inner').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false
+  });
+
+  //xl items main-page slider
+  $('.xl__slider-inner').slick({
+    autoplay: true,
+    autoplaySpeed: 3000
+
+  });
+
+  $('.followed__items').slick({
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false
+
+  });
+
+
+
+  var mixer = mixitup('.s__items-inner');
+
 });
